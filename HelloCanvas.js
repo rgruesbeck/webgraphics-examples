0c9bde79ab2b1617cf0f068a82bcfd9e20ac9ffa
Ron Gruesbeck <rongruesbeck@gmail.com>
2020-03-06 16:37:19 -0800

    update README.md

--- HelloCanvas.js
+++ HelloCanvas.js
@@ -1,15 +1,18 @@
 function main() {
+
+    // Retrieve <canvas> element
     let canvas = document.getElementById("webgl");
     if (!canvas) {
         return console.error("Browser does not support WebGL");
     }
 
+    // Get the rendering context for the WebGL
     let gl = canvas.getContext("webgl");
     if (!gl) {
         return console.error("Couldn't create webgl context");
     }
 
-    // Set color buffer to black
+    // Specify the color for clearing <canvas>
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
     // Clear context with color buffer
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Web Graphics Examples
+
+[WebGL Programming Guide Examples](https://sites.google.com/site/webglbook/home)
+[The Book of Shaders](https://thebookofshaders.com/)
